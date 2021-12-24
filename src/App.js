c79b16f529d412e9f21b4a87e849383798b25663
Sakif Hridoy <sakifworkspace@gmail.com>
2021-12-24 16:16:33 +0600

    setup dependencies commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {ChatEngine} from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+  return (<ChatEngine 
+  
+    height="100vh"
+    projectID="76f18c9f-080e-46de-b226-98949ce00eb1"
+    userName=""
+    userSecret=""
+
+  />
+  )
+}
+
+export default App;
