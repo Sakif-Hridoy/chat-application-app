6700bafe49c8c7331a6bb269bd799a4826eb8298
Sakif Hridoy <sakifworkspace@gmail.com>
2021-12-25 19:18:39 +0600

    access messages,last sent mesages(/'s) and my message with an unique key (/id) commit

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -11,6 +11,22 @@ const ChatFeed = (props)=>{
 const renderChatMessages = ()=>{
     const keys = Object.keys(messages);
     // console.log(keys);
+    return keys.map((key,index)=>{
+        const message = messages[key];
+        const lastMessageKey = index === 0 ? null : keys[index-1];
+        const isMyMessage = userName === message.sender.username;
+        return(
+            <div key={`msg_${index}`} style={{width:'100%'}}>
+                <div className="messgae-block">
+                    {
+                        isMyMessage
+                        ? <MyMessage/>
+                        :<TheirMessage/>
+                    }
+                </div>
+            </div>
+        )
+    })
     
 }
 renderChatMessages();
